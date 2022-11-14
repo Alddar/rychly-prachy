6dcb48b8fda66622a852c5d428941994c04ec050
Matthew <matej.karp@gmail.com>
2022-11-14 15:23:58 +0000

    MUI library init, layout added, basic structure added

--- ni-nur-semestral-brigady/pages/_app.tsx
+++ ni-nur-semestral-brigady/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "../styles/theme/theme";
+
+const createEmotionCache = () => {
+  return createCache({ key: "css", prepend: true });
+};
+
+const clientSideEmotioNCache = createEmotionCache();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider value={clientSideEmotioNCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
