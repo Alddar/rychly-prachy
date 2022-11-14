65e944271a1eabc0014d9b84d01cb1f08d0633ef
Ondrej Zavodny <ozavodny@purestorage.com>
2022-11-14 16:37:45 +0100

    stuff

--- ni-nur-semestral-brigady/components/layout.tsx
+++ ni-nur-semestral-brigady/components/layout.tsx
@@ -1,8 +1,37 @@
 import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export default function Layout({ children }: { children: any }) {
   return (
     <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ ml: 2 }}
+          >
+            <AccountCircleIcon />
+          </IconButton>
+          <div style={{ flexGrow: 1 }}></div>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <main>
         <Container>{children}</Container>
       </main>
