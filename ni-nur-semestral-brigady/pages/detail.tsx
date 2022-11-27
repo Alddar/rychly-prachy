1c828e238a29ce4ee1aca9e6f920645edb39c98a
matthew <matej@yolkstudio.com>
2022-11-27 10:12:57 +0100

    fix import err

--- ni-nur-semestral-brigady/pages/detail.tsx
+++ ni-nur-semestral-brigady/pages/detail.tsx
@@ -1,5 +1,5 @@
 import Typography from "@mui/material/Typography";
-import Container from '@mui/material/Container';
+import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import { useState } from "react";
@@ -9,33 +9,31 @@ import { DateTime } from "luxon";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import { LatLngExpression } from "leaflet";
-import Map from '../components/Map';
+import Map from "../components/map";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
-import StarIcon from '@mui/icons-material/StarOutlined';
-import MailIcon from '@mui/icons-material/MailOutline';
-import PhoneIcon from '@mui/icons-material/Phone';
+import StarIcon from "@mui/icons-material/StarOutlined";
+import MailIcon from "@mui/icons-material/MailOutline";
+import PhoneIcon from "@mui/icons-material/Phone";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 
-
 export default function Filter() {
   const offer: Offer = {
-    title: 'Rychlý prachy',
-    description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Nam sed tellus id magna elementum tincidunt. In convallis. Nulla non lectus sed nisl molestie malesuada. Donec quis nibh at felis congue commodo. In enim a arcu imperdiet malesuada.",
+    title: "Rychlý prachy",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam ligula pede, sagittis quis, interdum ultricies, scelerisque eu. Nam sed tellus id magna elementum tincidunt. In convallis. Nulla non lectus sed nisl molestie malesuada. Donec quis nibh at felis congue commodo. In enim a arcu imperdiet malesuada.",
     distance: 5,
-    requirements: [
-      "Pouze ženy",
-    ],
-    location: 'Praha',
+    requirements: ["Pouze ženy"],
+    location: "Praha",
     date: DateTime.local(2022, 5, 12, 16, 30),
     price: 500,
-    duration: 0.05
-  }
+    duration: 0.05,
+  };
 
-  const position: LatLngExpression = [50.069610, 14.374329]
+  const position: LatLngExpression = [50.06961, 14.374329];
 
-  const [ isInterestGiven, setInterestGiven ] = useState(false);
+  const [isInterestGiven, setInterestGiven] = useState(false);
 
   return (
     <Container maxWidth="sm" sx={{ paddingBottom: "2rem" }}>
@@ -44,40 +42,56 @@ export default function Filter() {
         <Typography>{offer.description}</Typography>
         <Typography variant="h5">Požadavky</Typography>
         <List>
-          { offer.requirements.map((requirement, i) => 
+          {offer.requirements.map((requirement, i) => (
             <ListItem disablePadding key={i}>
               {requirement}
-            </ListItem> 
-          ) }
+            </ListItem>
+          ))}
         </List>
         <Typography variant="h5">Lokalita</Typography>
-        <Map className="detail-map" center={position} zoom={15} scrollWheelZoom={false}>
+        <Map
+          className="detail-map"
+          center={position}
+          zoom={15}
+          scrollWheelZoom={false}
+        >
           {({ TileLayer, Marker, Popup }) => (
             <>
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={position}>
-              </Marker>
+              <Marker position={position}></Marker>
             </>
           )}
         </Map>
-        { !isInterestGiven ? <>
+        {!isInterestGiven ? (
+          <>
             <Typography variant="h5">Zadavatel</Typography>
-            <Box sx={{ display: 'flex' }}>
-              <Avatar sx={{ bgcolor: '#fb3', marginRight: '1rem' }}>M</Avatar>
-              <Box sx={{display: 'flex', flexDirection: 'column'}}>
+            <Box sx={{ display: "flex" }}>
+              <Avatar sx={{ bgcolor: "#fb3", marginRight: "1rem" }}>M</Avatar>
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
                 <Typography>Anonymní Borec</Typography>
-                <Typography variant="caption">1.1 <StarIcon sx={{ fontSize: 11 }} /></Typography>
+                <Typography variant="caption">
+                  1.1 <StarIcon sx={{ fontSize: 11 }} />
+                </Typography>
               </Box>
             </Box>
-            <Button variant="contained" onClick={() => setInterestGiven(true)}>Projevit zájem a získat kontakt</Button>
-          </> : <>
+            <Button variant="contained" onClick={() => setInterestGiven(true)}>
+              Projevit zájem a získat kontakt
+            </Button>
+          </>
+        ) : (
+          <>
             <Typography variant="h5">Kontakt</Typography>
             <List disablePadding>
               <ListItem disablePadding>
-                <Typography variant="h6">Anonymní Borec <Typography variant="caption">1.1 <StarIcon sx={{ fontSize: 11 }} /></Typography></Typography>
+                <Typography variant="h6">
+                  Anonymní Borec{" "}
+                  <Typography variant="caption">
+                    1.1 <StarIcon sx={{ fontSize: 11 }} />
+                  </Typography>
+                </Typography>
               </ListItem>
               <ListItem disablePadding>
                 <ListItemIcon>
@@ -92,9 +106,11 @@ export default function Filter() {
                 <ListItemText primary="+420 123 456 789" />
               </ListItem>
             </List>
-            <Button variant="contained" onClick={() => setInterestGiven(false)}>Zrušit zájem</Button>
-          </> 
-        }
+            <Button variant="contained" onClick={() => setInterestGiven(false)}>
+              Zrušit zájem
+            </Button>
+          </>
+        )}
       </Stack>
     </Container>
   );
